6a1c71d375017da047be15343ae87615e04c589d
Anjana Kavindu <anjanakavinducampus@gmail.com>
2024-07-01 14:51:08 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MainComponent } from './main/main.component'
 import { NavigationComponent } from './navigation/navigation.component'
+import { ContentComponent } from './content/content.component'
 import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, MainComponent, NavigationComponent],
+    imports: [RouterOutlet, MainComponent, NavigationComponent, ContentComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
