aa590fa92e22088e40ec2f943108a4ffdb0ea7dc
Anjana Kavindu <anjanakavinducampus@gmail.com>
2024-07-01 13:24:47 +0530

    create new component

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-component',
+    standalone: true,
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.css'
+})
+
+export class MainComponent {
+
+}
